3a4627ce8c66d438b85e9de56a457ed608b161ba
Hin Lui <luihin903@gmail.com>
2025-12-14 19:48:26 -0800

    Complete

---
+++ README.md
@@ -0,0 +1,33 @@
+# Download audio from YouTube with customizable adjustments
+
+## Requirements
+
+- FFmpeg
+- cookies.txt (Use https://chrome.google.com/webstore/detail/get-cookiestxt-locally/cclelndahbckbenkjhflpdbgdldlbecc) (You may need to update frequently)
+
+## Arguments
+
+- Target URL
+- Artist
+- Title
+- Album
+- Seconds to pad before starting (negative value to trim)
+- Seconds to pad after ending (negative value to trim)
+
+## How To Run
+
+- `git clone https://github.com/luihin903/YouTube-Audio-Downloader.git`
+- `cd YouTube-Audio-Downloader`
+- `python -m venv venv`
+- `venv\Scripts\activate`
+- `pip install -r requirements.txt`
+- `python main.py [Target URL] [Artist] [Title] [Album] [Seconds after starts] [Seconds before ends]`
+
+## How It Works
+
+- Retrieve the audio from YouTube
+- Pad/trim the beginning
+- Pad/trim the ending
+- Trim to the smaller second
+- Normalize volume (across files)
+- Edit metadata/tags
---
+++ main.py
@@ -0,0 +1,69 @@
+import re
+import yt_dlp
+from pydub import AudioSegment
+from mutagen.easyid3 import EasyID3
+import os
+import sys
+
+def process(index: int) -> tuple[str, str]:
+    arg = sys.argv[index]
+    return re.sub(r"[<>:\"/\\|?*]", '_', arg), arg
+
+url = sys.argv[1]
+artist, artist_raw = process(2)
+title, title_raw = process(3)
+album, album_raw = process(4)
+start = int(sys.argv[5])
+end = int(sys.argv[6])
+
+ydl_opts = {
+    "format" : "bestaudio/best",
+    "outtmpl" : f"[raw] {artist} - {title} ({album})",
+    "postprocessors" : [
+        {
+            "key" : "FFmpegExtractAudio",
+            "preferredcodec" : "mp3",
+            "preferredquality" : "128"
+        }
+    ],
+    "quiet" : True,
+    "no_warnings" : True,
+    "cookiefile" : "cookies.txt"
+}
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl: # type: ignore
+    ydl.download([url])
+
+audio = AudioSegment.from_file(f"[raw] {artist} - {title} ({album}).mp3")
+
+# pad starting
+duration = len(audio)
+if start <= 0:
+    audio = audio[-start*1000:]
+else:
+    padding = AudioSegment.silent(duration = start*1000)
+    audio = padding + audio
+
+# pad ending
+duration = len(audio)
+if end <= 0:
+    audio = audio[:end*1000]
+else:
+    padding = AudioSegment.silent(duration = end*1000)
+    audio = audio + padding
+
+# others
+duration = len(audio)
+audio = audio[:-(duration%1000)]
+volume_change = -10 - audio.dBFS
+audio.apply_gain(volume_change)
+audio.export(f"{artist} - {title} ({album}).mp3", format = "mp3", bitrate = "128k")
+
+# Edit metadata / tags
+audio = EasyID3(f"{artist} - {title} ({album}).mp3")
+audio["artist"] = artist_raw
+audio["title"] = title_raw
+audio["album"] = album_raw
+audio.save()
+
+os.remove(f"[raw] {artist} - {title} ({album}).mp3")
